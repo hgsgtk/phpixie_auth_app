408748d9bc73ba3f93516dfcb4877c188608c820
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2017-11-18 18:35:44 +0900

    initial commit

---
+++ assets/config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+ 	'default' => array(
+        'database' => 'phpixie',
+        'user'     => 'root',
+        'password' => 'P@ssw0rd',
+        'adapter'  => 'mysql', // one of: mysql, pgsql, sqlite
+        'driver'   => 'pdo'
+    )
+);
